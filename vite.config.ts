a56ee631a10e861adf2f598b6f128e5db69ef14a
ZayGo1ted <otmaneaboulouafa40@gmail.com>
2026-01-13 21:30:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    // This mapping ensures process.env.API_KEY is available in the browser context
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
     'process.env': process.env
   }
